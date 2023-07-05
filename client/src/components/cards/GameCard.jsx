8348d69bb34b067e3d9cdd1d497dd63912dd7fda
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-05 21:18:42 +0200

    Format refactor

--- client/src/components/cards/GameCard.jsx
+++ client/src/components/cards/GameCard.jsx
@@ -8,7 +8,9 @@ const GameCard = ({ games }) => {
 		<div className="m-6">
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 items-center justify-center text-white font-extrabold gap-7">
 				{games.map((item) => (
-					<div key={item.id} className="relative transition duration-300 hover:scale-110 cursor-pointer">
+					<div
+						key={item.id}
+						className="relative transition duration-300 hover:scale-110 cursor-pointer">
 						<img
 							src={item.background_image}
 							alt={item.name}
@@ -16,19 +18,27 @@ const GameCard = ({ games }) => {
 						/>
 						<div className="absolute bottom-0 bg-gradient-to-t from-black via-black/80 to-black/30 h-full w-full flex flex-col gap-4 py-3 px-6 justify-end items-start">
 							<div>
-								<Typography className="text-white whitespace-normal " sx={{ fontSize : '14px'}}>{item.name}</Typography>
+								<Typography
+									className="text-white whitespace-normal "
+									sx={{ fontSize: "14px" }}>
+									{item.name}
+								</Typography>
 								{/* <Rating value={parseInt(item.rating)} precision={0.5} readOnly size='small'/> */}
-								<Typography className="text-neutral-400  " sx={{ fontSize : '13px'}} >
+								<Typography
+									className="text-neutral-400  "
+									sx={{ fontSize: "13px" }}>
 									<span className="text-purple-600 font-bold">
 										{item.ratings_count}{" "}
 									</span>
 									Rating Count
 								</Typography>
-								<p className="font-light text-sm text-neutral-400">Released on {item.released}</p>
+								<p className="font-light text-sm text-neutral-400">
+									Released on {item.released}
+								</p>
 							</div>
 							<Link to={`/games/${item.id}`} className="w-full">
 								<button className="bg-transparent border border-gray-500 py-2  w-full transition duration-100 active:scale-95 whitespace-nowrap hover:bg-neutral-700/20">
-									<Typography sx={{ fontSize : '13px'}}>View Game</Typography>
+									<Typography sx={{ fontSize: "13px" }}>View Game</Typography>
 								</button>
 							</Link>
 						</div>
