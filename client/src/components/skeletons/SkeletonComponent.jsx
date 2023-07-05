09d53fdd5c1cdce60469289251edbc3060043d2b
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-07-05 21:18:26 +0200

    Format refactor

--- client/src/components/skeletons/SkeletonComponent.jsx
+++ client/src/components/skeletons/SkeletonComponent.jsx
@@ -3,13 +3,34 @@ import { Skeleton } from "@mui/material";
 const SkeletonComponent = () => {
 	return (
 		<div className="flex flex-col gap-2 bg-neutral-900 p-3">
-			<Skeleton variant="rectangular" width="100%" height={'200px'} sx={{ bgcolor : '#202020'}} animation="wave" />
+			<Skeleton
+				variant="rectangular"
+				width="100%"
+				height={"200px"}
+				sx={{ bgcolor: "#202020" }}
+				animation="wave"
+			/>
 			<div className="flex gap-2 pt-2">
-				<Skeleton variant="circular" width={'60px'} height={'50px'} sx={{bgcolor : '#202020'}} animation="wave"/>
+				<Skeleton
+					variant="circular"
+					width={"60px"}
+					height={"50px"}
+					sx={{ bgcolor: "#202020" }}
+					animation="wave"
+				/>
 				<p className="w-full">
-					<Skeleton variant="text" width={"100%"} sx={{ bgcolor : '#202020'}} animation="wave"/>
-					<Skeleton variant="text" width={"100%"} sx={{ bgcolor : '#202020'}} animation="wave"/>
-
+					<Skeleton
+						variant="text"
+						width={"100%"}
+						sx={{ bgcolor: "#202020" }}
+						animation="wave"
+					/>
+					<Skeleton
+						variant="text"
+						width={"100%"}
+						sx={{ bgcolor: "#202020" }}
+						animation="wave"
+					/>
 				</p>
 			</div>
 		</div>
